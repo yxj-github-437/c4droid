01dadf519ce01b22fd7f8dee773afacf5a97b5f9
yxj-github-437 <85388151+yxj-github-437@users.noreply.github.com>
2021-07-11 22:54:02 +0800

    Add files via upload

--- Qt_project/Chess/main.cpp
+++ Qt_project/Chess/main.cpp
@@ -1,1 +1,11 @@
+#include <QApplication>
+#include<QWidget>
+#include "Board.h"
 
+int main(int argc, char** argv) {
+    QApplication app(argc,argv);
+    Board board;
+    board.show();
+
+    return app.exec();
+}
