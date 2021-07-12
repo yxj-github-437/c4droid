356422531f850ac077246257d954ede97a1f8742
yxj-github-437 <85388151+yxj-github-437@users.noreply.github.com>
2021-07-12 08:00:50 -0500

    Add files via upload

--- Qt_project/test001/qthelloworld.cpp
+++ Qt_project/test001/qthelloworld.cpp
@@ -1,1 +1,11 @@
+#include <QApplication>
+#include <QLabel>
+ 
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);    
+    QLabel *label = new QLabel("Hello, world!");    
+    label->show();
+    return app.exec();
+}
 
+#include <qthelloworld.cpp.moc>
